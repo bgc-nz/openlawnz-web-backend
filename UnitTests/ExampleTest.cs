6f63cc7541741f457195ef4a17177bf3c068cd78
Benjamin Cottrell <me@bgcottrell.com>
2020-01-23 22:40:56 +0000

    Implemented unit testing

---
+++ UnitTests/ExampleTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+namespace aspnetapp.Tests
+{
+    [TestFixture]
+    class ExampleTest
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.AreEqual(2,2);
+        }
+    }
+}
